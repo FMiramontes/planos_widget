acaca9cabb7bdd408344b83028fce9b95fcbb3d9
Fernando Miramontes <ffernandoandresmiramontesramire@gmail.com>
2022-04-28 18:33:50 -0700

    Se agrega funcionalidad del OCR en titular 2

--- app/js/ocr.js
+++ app/js/ocr.js
@@ -1,71 +1,107 @@
 console.log('Running OCR...')
 
 // Register listener for successful scan event to obtain results
-const el = document.querySelector('blinkid-in-browser')
-console.log(el)
+const t1 = document.getElementById('scan-titular2')
 
-el.addEventListener('scanSuccess', (ev) => {
+// scan-titular1
+
+t1.addEventListener('scanSuccess', (ev) => {
     // Since UI component uses custom events, data is placed in `detail` property
     console.log('Results', ev.detail)
 
-    getDetails(ev.detail.recognizer)
+    testOCR.getDetails(ev.detail.recognizer)
 })
 
-function getDetails(ocr) {
-    /* Nombre */
-    const First_Name = ocr.fullName
-    const Apellido_Paterno = ocr.fathersName
-    const Apellido_Materno = ocr.mothersName
 
-    /* Domicilio */
-    const address = ocr.address
-    const splitAddress = address.split('\n')
+// ###################
+// scan-titular2
 
-    const calle = splitAddress[0]
-    const Colonia = splitAddress[1].replace(/\d+/g, '')
-    const Mailing_Zip = splitAddress[1].replace(/\D+/g, '')
-    const Mailing_City = splitAddress[2].split(' ,')[0]
+const t2 = document.getElementById('scan-titular2')
 
-    /* Nacimiento */
-    const diaNacimiento = ocr.dateOfBirth.day
-    const mesNacimiento = ocr.dateOfBirth.month
-    const a_oNacimiento = ocr.dateOfBirth.year
-    const Date_of_Birth =
-        ocr.succesfullyParsed == true
-            ? ocr.originalString
-            : `${diaNacimiento}/${mesNacimiento}/${a_oNacimiento}`
+t2.addEventListener('scanSuccess', (ev) => {
+    // Since UI component uses custom events, data is placed in `detail` property
+    console.log('Results', ev.detail)
 
-    const CURP = ocr.personalIdNumber
-
-    const data = {
-        First_Name,
-        Apellido_Paterno,
-        Apellido_Materno,
-        calle,
-        Colonia,
-        Mailing_Zip,
-        Mailing_City,
-        Date_of_Birth,
-        CURP,
-    }
-    testOCR.paintDataInForm(data)
-}
-
-// let containerWrap = document.querySelector('.container-wrap')
-// const btnTest = document.getElementById('test')
-// btnTest.addEventListener('click', (e) => {
-//     containerWrap.classList.add('show')
-// })
-
-// let containerModal = document.querySelector('.container-modal')
-
-// containerModal.addEventListener('click', (event) => {
-//     if (event.target == containerModal) {
-//         containerWrap.classList.remove('show')
-//     }
-// })
+    testOCR.getDetails2(ev.detail.recognizer)
+})
 
 const testOCR = {
+    getDetails(ocr) {
+        /* Nombre */
+        const First_Name = ocr.fullName
+        const Apellido_Paterno = ocr.fathersName
+        const Apellido_Materno = ocr.mothersName
+    
+        /* Domicilio */
+        const address = ocr.address
+        const splitAddress = address.split('\n')
+    
+        const calle = splitAddress[0]
+        const Colonia = splitAddress[1].replace(/\d+/g, '')
+        const Mailing_Zip = splitAddress[1].replace(/\D+/g, '')
+        const Mailing_City = splitAddress[2].split(' ,')[0]
+    
+        /* Nacimiento */
+        const Lugar_de_nacimiento = ocr.placeOfBirth
+        const diaNacimiento = ocr.dateOfBirth.day
+        const mesNacimiento = ocr.dateOfBirth.month
+        const a_oNacimiento = ocr.dateOfBirth.year
+        const Date_of_Birth = `${a_oNacimiento}-${mesNacimiento}-${diaNacimiento}`
+    
+        const CURP = ocr.personalIdNumber
+    
+        const data = {
+            First_Name,
+            Apellido_Paterno,
+            Apellido_Materno,
+            calle,
+            Colonia,
+            Mailing_Zip,
+            Mailing_City,
+            Lugar_de_nacimiento,
+            Date_of_Birth,
+            CURP,
+        }
+        testOCR.paintDataInForm(data)
+    },
+    getDetails2(ocr) {
+        /* Nombre */
+        const Nombre2 = ocr.fullName
+        const ApellidoP2 = ocr.fathersName
+        const Apellido_Materno_2 = ocr.mothersName
+    
+        /* Domicilio */
+        const address = ocr.address
+        const splitAddress = address.split('\n')
+    
+        const Calle2 = splitAddress[0]
+        const Colonia2 = splitAddress[1].replace(/\d+/g, '')
+        const Zip_SegundoCliente = splitAddress[1].replace(/\D+/g, '')
+        const Ciudad2 = splitAddress[2].split(' ,')[0]
+    
+        /* Nacimiento */
+        const LUGAR_DE_NACIMIENTO_2 = ocr.placeOfBirth
+        const diaNacimiento = ocr.dateOfBirth.day
+        const mesNacimiento = ocr.dateOfBirth.month
+        const a_oNacimiento = ocr.dateOfBirth.year
+        const Fecha_Nacimiento2 = `${a_oNacimiento}-${mesNacimiento}-${diaNacimiento}`
+    
+        const CURP2 = ocr.personalIdNumber
+    
+        const data = {
+            Nombre2,
+            ApellidoP2,
+            Apellido_Materno_2,
+            Calle2,
+            Colonia2,
+            Zip_SegundoCliente,
+            Ciudad2,
+            LUGAR_DE_NACIMIENTO_2,
+            Fecha_Nacimiento2,
+            CURP2,
+        }
+        testOCR.paintDataInForm(data)
+    },
     async paintDataInForm(data) {
 
         // pintar informacion en Formulario
